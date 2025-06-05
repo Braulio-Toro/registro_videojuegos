3260960abb5de70bfd77972be3ae2296d724bb92
Braulio-Toro <br.toropalma@duocuc.cl>
2025-06-05 10:41:00 -0400

    Paso 1: Estructura del menú y tupa de plataformas creada

---
+++ videojuegos.py
@@ -0,0 +1,16 @@
+#Consolas de videojuegos
+
+menu=f"""--- MENÚ DE VIDEOJUEGOS ---
+1) Registrar videojuegos
+2) Ver videojuegos
+3) Modificar videojuego
+4) Eliminar videojuego
+5) Salir
+{"-"*30}"""
+videojuegos=[]
+
+plataformas=("PC", "PS5", "Xbox Series X", "Nimtendo Switch")
+
+while True:
+    print(menu)
+    break
