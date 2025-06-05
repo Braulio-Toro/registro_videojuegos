1fcea2c0d3fa1fb5ea7bcfdee641831bb1045658
Braulio-Toro <br.toropalma@duocuc.cl>
2025-06-05 10:51:53 -0400

    Paso 2: Registro de videojuego

--- videojuegos.py
+++ videojuegos.py
@@ -1,6 +1,7 @@
 #Consolas de videojuegos
+import os, time, msvcrt
 
-menu=f"""--- MENÚ DE VIDEOJUEGOS ---
+menu=f"""\n--- MENÚ DE VIDEOJUEGOS ---
 1) Registrar videojuegos
 2) Ver videojuegos
 3) Modificar videojuego
@@ -12,5 +13,37 @@ videojuegos=[]
 plataformas=("PC", "PS5", "Xbox Series X", "Nimtendo Switch")
 
 while True:
+    os.system("cls || clear")
     print(menu)
-    break
+    opc=input("Ingrese una opción (1-5): ")
+    os.system("cls || clear")
+    if opc=="1":
+        codigo=int(input("Ingrese el código del videojuego: "))
+        nombre=input("Ingrese el nombre del videojuego: ")
+        genero=input("Ingrese el género del videojuego: ")
+        print(f"""PLATAFORMAS DISPONIBLES: 
+              1) PC
+              2) PS5
+              3) Xbox Series X
+              4) Nintendo Switch
+              """)
+        plataforma_codigo=int(input("Seleccione el número de la plataforma: "))
+        plataforma=plataformas[plataforma_codigo - 1]
+        videojuego={
+            "codigo":codigo,
+            "nombre":nombre,
+            "genero":genero,
+            "plataforma":plataforma
+        }
+        videojuegos.append(videojuego)
+        print("Videojuego registrado correctamente.")
+    elif opc=="2":
+        pass
+    elif opc=="3":
+        pass
+    elif opc=="4":
+        print("Saliendo del programa.")
+        break
+    else:
+        print("Opción invalida")
+        
